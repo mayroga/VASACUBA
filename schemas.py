581bc9c0552101ef39403710f4fd8b06c93d1da5
mayroga <132472344+mayroga@users.noreply.github.com>
2026-09-25 17:55:17 -0400

    Remove spaces around type hints in schemas.py

--- schemas.py
+++ schemas.py
@@ -2,26 +2,26 @@ from typing import Optional
 from pydantic import BaseModel,Field
 
 class VisaRequest(BaseModel):
-    nationality: Optional[str]=Field(default=None)
-    residence: Optional[str]=Field(default=None)
-    travel_purpose: Optional[str]=Field(default=None)
-    visa_type: Optional[str]=Field(default=None)
-    airline: Optional[str]=Field(default=None)
-    origin: Optional[str]=Field(default=None)
-    destination: Optional[str]=Field(default="Cuba")
+    nationality:Optional[str]=Field(default=None)
+    residence:Optional[str]=Field(default=None)
+    travel_purpose:Optional[str]=Field(default=None)
+    visa_type:Optional[str]=Field(default=None)
+    airline:Optional[str]=Field(default=None)
+    origin:Optional[str]=Field(default=None)
+    destination:Optional[str]=Field(default="Cuba")
 
 class DViajerosRequest(BaseModel):
-    nationality: Optional[str]=Field(default=None)
-    residence: Optional[str]=Field(default=None)
-    travel_date: Optional[str]=Field(default=None)
-    origin: Optional[str]=Field(default=None)
-    destination: Optional[str]=Field(default="Cuba")
-    purpose: Optional[str]=Field(default=None)
+    nationality:Optional[str]=Field(default=None)
+    residence:Optional[str]=Field(default=None)
+    travel_date:Optional[str]=Field(default=None)
+    origin:Optional[str]=Field(default=None)
+    destination:Optional[str]=Field(default="Cuba")
+    purpose:Optional[str]=Field(default=None)
 
 class PassportRequest(BaseModel):
-    nationality: Optional[str]=Field(default=None)
-    passport_type: Optional[str]=Field(default=None)
-    passport_status: Optional[str]=Field(default=None)
-    expiration_date: Optional[str]=Field(default=None)
-    destination: Optional[str]=Field(default="Cuba")
+    nationality:Optional[str]=Field(default=None)
+    passport_type:Optional[str]=Field(default=None)
+    passport_status:Optional[str]=Field(default=None)
+    expiration_date:Optional[str]=Field(default=None)
+    destination:Optional[str]=Field(default="Cuba")
 
